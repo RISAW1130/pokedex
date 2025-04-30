3f9095ce7dc488fdc7c691b8849dea9fd44301f4
Risa Watanabe <rwatanabe@smith.edu>
2025-04-30 08:40:15 -0400

    pokemon_photo function code complete

--- R/pokemon_photo.R
+++ R/pokemon_photo.R
@@ -1,13 +1,14 @@
 #' @title Plot specified Pokemon's PNG image
 #' @description
-#' `pokemon_photo()` displays a PNG image of a specified Pokemon (excluding variations in different Forms, Mega Evolutions, Region Forms) as a plot in a graphics device (e.g., the RStudio plot window).
+#' `pokemon_photo()` displays a PNG image of a specified Pokemon as a plot in a graphics device (e.g., the RStudio plot window).
 #' @param name A character object that is the name of the Pokemon
 #' @importFrom png readPNG
 #' @importFrom graphics plot.new
 #' @importFrom grid grid.raster
-#' @returns A rastergrob. See "Details" section of [grid::grob()] for more information.
+#' @returns A raster object (bitmap image).
 #' @examples
 #' pokemon_photo("Pikachu")
+#' pokemon_photo("Mega Absol")
 #' @export
 pokemon_photo <- function(name) {
   num <- NA
@@ -17,21 +18,34 @@ pokemon_photo <- function(name) {
   }
 
   for (i in 1:nrow(pokedex_data)) {
-    if (tolower(pokedex_data$Original_Name[i]) == tolower(name)) {
-      num <- as.character(pokedex_data$No[i])
+    if (tolower(pokedex_data$Name[i]) == tolower(name)) {
+      if (stringr::str_sub(pokedex_data$Branch_Code[i], start = -2) == "_0"){
+
+        num <- as.character(pokedex_data$No[i])
+
+        if (nchar(num) == 1) {
+          num <- paste0("00", num)
+        }
+        if (nchar(num) == 2) {
+          num <- paste0("0", num)
+        }
+      } else {
+        variation <- as.numeric(stringr::str_sub(pokedex_data$Branch_Code[i], start = -1))
+
+        num <- paste0(pokedex_data$No[i], "_f", variation + 1)
+
+        if (nchar(pokedex_data$No[i]) == 1) {
+          num <- paste0("00", num)
+        }
+        if (nchar(pokedex_data$No[i]) == 2) {
+          num <- paste0("0", num)
+        }
+      }
     }
   }
 
   if (is.na(num) == TRUE) {
     stop("We could not find a matching Pokemon in our Pokedex. Try again.")
-  }
-
-  if (nchar(num) == 1) {
-    num <- paste0("00", num)
-  }
-
-  if (nchar(num) == 2) {
-    num <- paste0("0", num)
   }
 
   url <- paste0("https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/", num, ".png")
