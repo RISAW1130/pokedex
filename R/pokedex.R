28b23dfb639b5351b22569bb280cd442e1408d33
Rose Ordonez-Rodriguez <rordonezrodriguez@smith.edu>
2025-05-02 13:49:58 -0400

    Committing Test Search Function

--- R/pokedex.R
+++ R/pokedex.R
@@ -1,9 +1,14 @@
 #' @title Use the Pokedex Type Search Function
 #' @description
 #' Given a comic number, the `pokedex()` function calls the Pokemon data set and returns the name of all of the Pokemon's that have Type 1 features that the user specified in the form of a list object.
-#' @importFrom read_csv
 #' @export
-search_pokedex <- function(type_input, pokedex_dataset) {
+#search_pokedex <- function(pokedex_data) {
+#  matches <- tolower(pokedex_data$type_1) == tolower(type_input)
+#  return(as.character(matches))
+#}
 
-}
+#tolower !!!!
+#' @examples
+
+#type in the element they want (but are restricted only to type 1) and it lists out the Pokemon names in return
 
