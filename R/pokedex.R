b8bcaa8cc8fcac9c5aa78f344f19d8f9cdabf987
Rose Ordonez-Rodriguez <rordonezrodriguez@smith.edu>
2025-05-04 12:08:12 -0400

    Committing Final Search Function

--- R/pokedex.R
+++ R/pokedex.R
@@ -1,9 +1,22 @@
 #' @title Use the Pokedex Type Search Function
 #' @description
-#' Given a comic number, the `pokedex()` function calls the Pokemon data set and returns the name of all of the Pokemon's that have Type 1 features that the user specified in the form of a list object.
-#' @importFrom read_csv
+#' The `search_pokedex()` function allows users to search for Pokémon based on their primary type (`Type1`), using a case-insensitive match. It returns a list of Pokémon names that match the specified type.
+#' @param type1 A character string specifying the primary Pokémon type to search for (e.g., "Fire", "Water", "Electric").
+#'
+#' @returns A list of character vectors containing the names of Pokémon whose `Type1` matches the input type.
 #' @export
 search_pokedex <- function(type1) {
   matching_pokemon <- pokedex_data[tolower(pokedex_data$Type1) == tolower(type1), ]
   return(as.list(matching_pokemon$Name))
 }
+
+#' @examples
+#' # Return a list of Fire-type Pokémon
+#' search_pokedex("Fire")
+#'
+#' # Case-insensitive search
+#' search_pokedex("fIrE")
+#'
+#' # No match returns an empty list
+#' search_pokedex("Banana")
+
